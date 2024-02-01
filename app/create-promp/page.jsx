b6802b9036fb4b3365b3515d3cf5190d7f9fb1b2
Hoda96 <farahani.h40@gmail.com>
2024-02-01 17:56:07 +0330

    Form Created

--- app/create-promp/page.jsx
+++ app/create-promp/page.jsx
@@ -1,8 +1,25 @@
 "use client";
+import Form from "@/components/Form";
 import { useState } from "react";
 
 function CreatePrompt() {
-  return <div>CreatePrompt</div>;
+  const [post, setPost] = useState({
+    prompt: " ",
+    tag: " ",
+  });
+  const [submitting, setSubmitting] = useState(false);
+
+  async function createPrompt(e) {}
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      submitting={submitting}
+      setPost={setPost}
+      handleSubmit={createPrompt}
+    />
+  );
 }
 
 export default CreatePrompt;
